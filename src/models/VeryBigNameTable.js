98927ec4e9a7f158a0a7350558a9e83a52f89be8
Josefa (Jitera) <developers@jitera.com>
2025-03-04 12:19:41 +0000

    feat[database]: delete tables 'table_with_very_big_name_with_lot_of_characters_for_test_purpos' Changes + '/src/models/VeryBigNameTable.js': deleted + '/src/migrations/1741089218574_create_table_with_very_big_name_with_lot_of_characters_for_test_purpos.js': deleted

--- src/models/VeryBigNameTable.js
+++
@@ -1,9 +0,0 @@
-class VeryBigNameTable {
-  constructor(id, created_at, updated_at) {
-      this.id = id;
-      this.created_at = created_at;
-      this.updated_at = updated_at;
-  }
-}
-
-module.exports = VeryBigNameTable;
